4f3412e2312ee4600aea0a4dc2275c06b02f59d4
Kevin Marquette <Kevin.Marquette@everi.com>
2016-08-19 11:41:44 -0500

    Setup module structure

---
+++ PSSSRS/psssrs.psm1
@@ -0,0 +1,19 @@
+﻿[String]$FTPServerTranscript = ""
+
+$moduleRoot = Split-Path -Path $MyInvocation.MyCommand.Path
+
+Write-Verbose "Importing Functions"
+# Import everything in the functions folders
+$publicFunctions = LS "$moduleRoot\public-functions\*.ps1" |
+  Where-Object { -not ($_.FullName.Contains(".Tests.")) } 
+ 
+$publicFunctions | ForEach-Object { Write-Verbose "Public Function: $($_.FullName)"; . $_.FullName} 
+  
+$privateFunctions = LS "$moduleRoot\private-functions\*.ps1" |
+  Where-Object { -not ($_.FullName.Contains(".Tests.")) } 
+ 
+$privateFunctions | ForEach-Object { Write-Verbose "Private Function: $($_.FullName)"; . $_.FullName} 
+  
+Write-Verbose 'Export all public functions'
+Export-ModuleMember -Function $publicFunctions.BaseName
+Export-ModuleMember -Variable FTPServerTranscript
