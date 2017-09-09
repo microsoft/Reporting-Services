f57f63b53793d82c7ab1f7a44c9c884ee5d97645
REDMOND\hali2 <hali2@microsoft.com>
2017-09-08 17:19:57 -0700

    add disclaimer statement

--- APISamples/powershell/powershellSamples.ps1
+++ APISamples/powershell/powershellSamples.ps1
@@ -1,3 +1,21 @@
+# Copyright (c) 2016 Microsoft Corporation. All Rights Reserved.
+# Licensed under the MIT License (MIT)
+
+<#============================================================================
+  File:     powershellSamples.ps1
+  Summary:  Demonstrates examples to upload/download/delete an item in RS. 
+--------------------------------------------------------------------
+    
+ This source code is intended only as a supplement to Microsoft
+ Development Tools and/or on-line documentation. See these other
+ materials for detailed information regarding Microsoft code 
+ samples.
+ THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF 
+ ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
+ THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+ PARTICULAR PURPOSE.
+===========================================================================#>
+
 $ReportPortalUri = 'http://myserver/reports/'
 
 Write-Host "Upload an item..."
@@ -8,24 +26,22 @@ $payload = @{
     "@odata.type" = "#Model.Report";
     "Content" = [System.Convert]::ToBase64String($bytes);
     "ContentType"="";
-    "Name" = 'chart';
-    "Path" = '/chart';
+    "Name" = 'test';
+    "Path" = '/test';
     } | ConvertTo-Json
 Invoke-WebRequest -Uri $catalogItemsUri -Method Post -Body $payload -ContentType "application/json" -UseDefaultCredentials | Out-Null
 
 
 Write-Host "Download an item..."
-$item = '/test'
 $downloadPath = 'C:\download\test.rdl'
-$catalogItemsApi = $ReportPortalUri + "/api/v2.0/CatalogItems(Path='{0}')/Content/$value"
+$catalogItemsApi = $ReportPortalUri + "/api/v2.0/CatalogItems(Path='/test')/Content/$value"
 $url = [string]::Format($catalogItemsApi, $item)
 $response = Invoke-WebRequest -Uri $url -Method Get -UseDefaultCredentials
 [System.IO.File]::WriteAllBytes($downloadPath, $response.Content)
 
 
 Write-Host "Delete an item..."
-$item = '/test'
-$url = $ReportPortalUri + "/api/v2.0/CatalogItems(Path='$item')"
+$url = $ReportPortalUri + "/api/v2.0/CatalogItems(Path='/test')"
 Invoke-WebRequest -Uri $url -Method Delete -UseDefaultCredentials | Out-Null
 
 
